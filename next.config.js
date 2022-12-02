112e8f02c88dea08703508356e38f6697b5812e3
farisan <muhammad.farisan99@gmail.com>
2022-12-02 18:39:49 +0700

    add env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.BACKEND_LEPISA,
+  },
 }
 
 module.exports = nextConfig
