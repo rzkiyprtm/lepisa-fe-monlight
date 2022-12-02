a5acb892dd0ea4eb4b0e7cd35b24f1913a48446f
farisan <muhammad.farisan99@gmail.com>
2022-12-03 00:45:38 +0700

    verifikasi

--- src/pages/auth/[otp].js
+++ src/pages/auth/[otp].js
@@ -1,14 +1,27 @@
-import React from 'react'
+import axios from 'axios';
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 
 import css from "../../styles/Auth.module.css"
 
 function Otp() {
+  const router = useRouter();
+
+  useEffect(() => {
+    axios.get(`${NEXT_PUBLIC_BACKEND_URL}/auth/verify/${router.query.otp}`)
+    .then(console.log("Activasi berhasil"))
+    .catch((err) => console.log(err))
+  }, [])
+
+  const toLogin = () => router.push("/login")
+  
   return (
     <>
       <div className={`container-fluid ${css.container}`}>
           <div className="d-flex flex-column justify-content-center align-items-center">
             <p className={css.verify}>Verifiy Account</p>
             <p className={`${css.success} text-success`}>Success</p>
+            <button className={css.login} onClick={toLogin}>Klik here to login</button>
         </div>
       </div>
     </>
